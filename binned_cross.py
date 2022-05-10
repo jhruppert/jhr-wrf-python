4ab49e9ce6a9d2cf208857ecb8721b285fb30f31
James Ruppert <jruppert.jr@gmail.com>
2022-05-10 16:05:00 -0500

    updates

--- binned_cross.py
+++ binned_cross.py
@@ -1,36 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Notebook to genereate binned cross sections from TC output
+# 
+# Assumes output is in a single netcdf file on pressure levels.
+# 
+# James Ruppert  
+# jruppert@ou.edu  
+# 4/23/22
+
 
 from netCDF4 import Dataset
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import sys
-#import subprocess
 
-#### Time selection
 
+# #### Bin variable selection
+
+# Indexing variable
+ivar_select = 'lwacre'
+# pw, vmf, rain, lwacre
+
+
+# #### Time selection
+
+nt=24
 t0 = 48
-t1 = t0+24
+t1 = t0+nt
 
 storm = 'haiyan'
 
 
-#### Directories
+# #### Directories
+
+# figdir = "/Users/jruppert/code/tc_figs/"
+figdir = "/home/jamesrup/figures/tc/ens/"+storm+'/'
 
 # main = "/Users/jamesruppert/code/tc_output/"
-#main = "/Users/jruppert/code/tc_output/"
+# main = "/Users/jruppert/code/tc_output/"
 main = "/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/wrfenkf/"
-figdir = "/home/jamesrup/idl/figures/tc/ens"
-#storm = subprocess.check_output("ls $main", shell=True, capture_output=True)
+#storm = get_ipython().getoutput('ls $main')
 # print(storm)
-#istorm=storm[0]
 
-# Ensemble members
-#memb = !ls $main/$istorm
+#istorm=storm[0]
 nums=np.arange(1,21,1); nums=nums.astype(str)
 nustr = np.char.zfill(nums, 2)
 memb_all=np.char.add('memb_',nustr)
 imemb=memb_all[0]
+#memb = get_ipython().getoutput('ls $main/$istorm')
+#imemb=memb[0]
 # print(main+istorm+'/'+imemb)
 
 datdir = main+storm+'/'+imemb+'/ctl/'
@@ -38,7 +58,7 @@ datdir = datdir+'post/d02/'
 print(datdir)
 
 
-#### Read variables
+# #### Read variables
 
 # Fill contour variable
 
@@ -63,10 +83,16 @@ cmax=4; cmin=-1.*cmax
 varfil_strat = Dataset(datdir+'strat.nc') # this opens the netcdf file
 strat_in = varfil_strat.variables['strat'][t0:t1,:,:,:]
 
+# LW-ACRE
+binfil = Dataset(datdir+'LWacre.nc') # this opens the netcdf file
+lwacre = binfil.variables['LWUPB'][t0:t1,:,:,:] # W/m2
+
 bv_shape = np.shape(binvar_f_in)
 print("Binvar shape: ",bv_shape)
 nt = bv_shape[0]
 nz = bv_shape[1]
+nx1 = bv_shape[2]
+nx2 = bv_shape[3]
 
 
 # Vertical coordinate
@@ -74,58 +100,101 @@ pres = varfil_main.variables['pres'][:] # Pa
 print("Vertical shape: ",np.shape(pres))
 
 
+
 # Line contour variable
 
 # Vertical motion
 varfil_cvar = Dataset(datdir+'W.nc') # this opens the netcdf file
-binvar_c_in = varfil_cvar.variables['W'][t0:t1,:,:,:]*1e2 # m/s --> mm/s
+binvar_c_in = varfil_cvar.variables['W'][t0:t1,:,:,:]*1e2 # m/s --> cm/s
 units_var2='cm/s'
 lcmin = -20; lcmax=20; lcint=2
 
 
-# Indexing variable
-binfil = Dataset(datdir+'PW.nc') # this opens the netcdf file
-ivar = binfil.variables['PW'][t0:t1,:,:,:]
-print("PW shape: ",np.shape(ivar))
+# #### Bin variable and settings
 
+if ivar_select == 'pw':
+    # PW
+    binfil = Dataset(datdir+'PW.nc') # this opens the netcdf file
+    ivar = binfil.variables['PW'][t0:t1,:,:,:]
+    fmin=35;fmax=80 # mm
+    step=1
+    bins=np.arange(fmin,fmax+step,step)
+    xlabel='Column water vapor [mm]'
+    log_x='linear'
+    
+elif ivar_select == 'rain':
+    # Rainfall rate
+    binfil = Dataset(datdir+'rainrate.nc') # this opens the netcdf file
+    ivar = binfil.variables['rainrate'][t0:t1,:,:,:]
+    fmin=0; fmax=140 # mm / hr
+    bins=10.**(np.arange(1,8,0.3)-4)
+    xlabel='Rainfall rate [mm/hr]'
+    log_x='log'
 
-#### Create PW Bins
+elif ivar_select == 'vmf':
+    # Vertical mass flux
+    dp=10000. # delta-p, Pa
+    g=9.81 # gravity, m/s2
+    wv_int = np.sum(binvar_c_in*1e-2,1) * dp/g
+    ivar = np.reshape(wv_int,(nt,1,nx1,nx2))
+    fmin=0; fmax=140 # mm / hr
+    bins=10.**(np.arange(1,8,0.3)-3)
+    # bins=np.flip(-1.*bins)
+    xlabel='Vertical mass flux [kg/m/s]'
+    log_x='log'
 
-fmin=35 # mm
-fmax=80
-step=1
-bins = np.arange(fmin,fmax,step)
+elif ivar_select == 'lwacre':
+    # LW-ACRE
+    ivar = lwacre
+    fmin=-100; fmax=200 # W/m2
+    step=5
+    bins=np.arange(fmin,fmax+step,step)
+    xlabel='LW-ACRE [W/m**2]'
+    log_x='linear'
+
+    
+print("Binvar shape: ",np.shape(ivar))
+print(bins)
 nbins = np.size(bins)
+print(nbins)
 
-#### Bin the target variable
 
-binvar_f = np.zeros((nbins,nt,nz)) # nbins, nt, nz
-binvar_c = np.zeros((nbins,nt,nz))
-binvar_strat = np.zeros((nbins,nt,3))
+# #### Bin the target variable
+
+binvar_f = np.zeros((nbins-1,nt,nz)) # nbins, nt, nz
+binvar_c = np.zeros((nbins-1,nt,nz))
+binvar_strat = np.zeros((nbins-1,nt,4))
+binvar_acre = np.zeros((nbins-1,nt))
 
 # for ibin in range(nbins):
 for itim in range(nt):
-    for ibin in range(nbins):
-        indices = ((ivar[itim,0,:,:] >= bins[ibin]-0.5*step) & (ivar[itim,0,:,:] < bins[ibin]+0.5*step)).nonzero()
+    for ibin in range(nbins-1):
+        # indices = ((ivar[itim,0,:,:] >= bins[ibin]-0.5*step) & (ivar[itim,0,:,:] < bins[ibin]+0.5*step)).nonzero()
+        indices = ((ivar[itim,0,:,:] >= bins[ibin]) & (ivar[itim,0,:,:] < bins[ibin+1])).nonzero()
         tmp_f = binvar_f_in[itim,:,indices[0],indices[1]]
         binvar_f[ibin,itim,:] = np.mean(tmp_f,axis=0,dtype=np.float64)
         tmp_c = binvar_c_in[itim,:,indices[0],indices[1]]
         binvar_c[ibin,itim,:] = np.mean(tmp_c,axis=0,dtype=np.float64)
+        tmp_acre = lwacre[itim,:,indices[0],indices[1]]
+        binvar_acre[ibin,itim] = np.mean(tmp_acre,axis=0,dtype=np.float64)
         tmp_strat = strat_in[itim,:,indices[0],indices[1]]
-        for istrat in range(3):
-            iindex = ((tmp_strat == (istrat+1))).nonzero()
+        for istrat in range(4):
+            iindex = ((tmp_strat == (istrat))).nonzero()
             # print(iindex)
-            binvar_strat[ibin,itim,0] = np.shape(iindex)[1]
+            binvar_strat[ibin,itim,istrat] = np.shape(iindex)[1]
 
-#### Time-average
+
+# #### Time-average
+
 
 binvar_f_mn = np.mean(binvar_f,axis=1)
 binvar_c_mn = np.mean(binvar_c,axis=1)
+binvar_acre_mn = np.mean(binvar_acre,axis=1)
 binvar_s_mn = np.mean(binvar_strat,axis=1)
 
 
-
-### Plotting routines
+# ---
+# ### Plotting routines
 
 font = {'family' : 'sans-serif',
         'weight' : 'normal',
@@ -136,65 +205,153 @@ matplotlib.rc('font', **font)
 
 ### CONTOUR PLOT
 
+
 # create figure
 fig = plt.figure(figsize=(14,8))
 ax = fig.add_subplot(111)
 
-ax.set_title('Binned plot')
+ax.set_title('Binned LW-CRF')
 ax.set_ylabel('Pressure [hPa]')
-ax.set_xlabel('Column water vapor [mm]')
+
+# bins=np.flip(-1.*bins)
 
 # fill contour
 nlevs=21
 inc=(cmax-cmin)/nlevs
 clevs = np.arange(cmin, cmax+inc, inc)
 pltvar=binvar_f_mn
-im = ax.contourf(bins, pres, np.transpose(pltvar), clevs, cmap='RdBu_r', alpha=0.6, \
+im = ax.contourf(bins[0:nbins-1], pres, np.transpose(pltvar), clevs, cmap='RdBu_r', alpha=0.8, \
                  extend='max', zorder=2)
 
 cbar = plt.colorbar(im, ax=ax, shrink=0.75)
 cbar.ax.set_ylabel(units_var1)
 ax.invert_yaxis()
 ax.set_yscale('log')
+ax.set_xscale(log_x)
 ax.yaxis.set_major_formatter(matplotlib.ticker.ScalarFormatter())
 ax.yaxis.set_minor_formatter(matplotlib.ticker.ScalarFormatter())
+ax.set_xlabel(xlabel)
 
 # ax2=ax.twinx()
-# im = ax.plot(bins, binvar_s_mn)
+# im = ax.plot(bins[0:nbins-1], binvar_s_mn)
 
 # line contour
-clevs = [1,2,5,10,50,100,500,1000,2000,3000]#np.arange(lcmin, lcmax, lcint)
+# clevs = np.arange(lcmin, lcmax, lcint)
+clevs = [0.1,0.5,1,2,5,10,50,100,500,1000,2000,3000]
 clevs = np.concatenate((-1*np.flip(clevs),clevs))
 cpltvar=binvar_c_mn
 # cpltvar=np.gradient(binvar_c_mn,10000,axis=1,edge_order=2)*-1e5
-im = ax.contour(bins, pres, np.transpose(cpltvar), clevs, colors='black', zorder=2)
+im = ax.contour(bins[0:nbins-1], pres, np.transpose(cpltvar), clevs, colors='black', zorder=2)
 ax.clabel(im, im.levels, inline=True, fontsize=13)
 
 # plt.show()
-plt.savefig(figdir+'myplot.png',dpi=200)
+plt.savefig(figdir+'lwcrf_compcross_'+imemb+'_'+ivar_select+'.png',dpi=200, facecolor='white')
 
 
-### LINE PLOT
+
+### PLOT OF CONV/STRAT SEPARATION
+
 
 # create figure
 fig = plt.figure(figsize=(14,4))
 ax = fig.add_subplot(111)
 
-ax.set_title('Binned plot')
-ax.set_ylabel('Cell ID')
-ax.set_xlabel('Column water vapor [mm]')
+ax.set_title('Conv/Strat Separation')
+ax.set_xlabel(xlabel)
+ax.set_xscale(log_x)
 
 # Conv/strat separation: varout = 1 if convective, = 2 if stratiform, = 3 other, = 0 if no rain
 
-pltvar=binvar_s_mn
+# Raw numbers
+# ax.set_ylabel('N cells')
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,0], ".k", label="No rain")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,1], "-r", label="Conv")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,2], "-b", label="Strat")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,3], "--b", label="Other")
+# plt.ylim(0, 1e4)
 
-plt.plot(bins, binvar_s_mn[:,0], "-b", label="sine")
-plt.plot(bins, binvar_s_mn[:,1], "-r", label="cosine")
-plt.plot(bins, binvar_s_mn[:,2], "-g", label="cosine")
+# As fraction of category-total
+# ax.set_ylabel('Fraction')# of category-total')
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,0]/np.sum(binvar_s_mn[:,0]) \
+#          , ".k", label="No rain")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,1]/np.sum(binvar_s_mn[:,1]) \
+#          , "-r", label="Conv")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,2]/np.sum(binvar_s_mn[:,2]) \
+#          , "-b", label="Strat")
+# plt.plot(bins[0:nbins-1], binvar_s_mn[:,3]/np.sum(binvar_s_mn[:,3]) \
+#          , "--b", label="Other")
+# plt.ylim(0, 0.06)
+
+# As fraction of all-rain-total
+ax.set_ylabel('Fraction')
+total=np.sum(binvar_s_mn[:,(1,2,3)])
+plt.plot(bins[0:nbins-1], binvar_s_mn[:,0]/total \
+         , ".k", label="Non-raining")
+plt.plot(bins[0:nbins-1], binvar_s_mn[:,1]/total \
+         , "-r", label="Conv")
+plt.plot(bins[0:nbins-1], binvar_s_mn[:,2]/total \
+         , "-b", label="Strat")
+plt.plot(bins[0:nbins-1], binvar_s_mn[:,3]/total \
+         , "--b", label="Other")
+plt.ylim(0, 0.2)
 
 plt.legend(loc="upper left")
-# plt.ylim(-1.5, 2.0)
 
-#plt.show()
-plt.savefig(figdir+'myplot2.png',dpi=200)
+# plt.show()
+plt.savefig(figdir+'convstrat_comp_'+imemb+'_'+ivar_select+'.png',dpi=200, facecolor='white')
+
+
+
+### LW-ACRE PLOT
+
+
+# create figure
+fig = plt.figure(figsize=(14,4))
+ax = fig.add_subplot(111)
+
+ax.set_title('LW-ACRE')
+ax.set_xlabel(xlabel)
+ax.set_xscale(log_x)
+
+pltvar=binvar_acre_mn
+
+ax.set_ylabel('ACRE [W/m**2]')
+plt.plot(bins[0:nbins-1], pltvar)
+# plt.ylim(0, 0.2)
+
+# plt.legend(loc="upper left")
+
+# plt.show()
+plt.savefig(figdir+'lwacre_comp_'+imemb+'_'+ivar_select+'.png',dpi=200, facecolor='white')
+
+
+
+### LW-ACRE SCALED PLOT
+
+
+# create figure
+fig = plt.figure(figsize=(14,4))
+ax = fig.add_subplot(111)
+
+ax.set_title('LW-ACRE * Rain Fraction')
+ax.set_xlabel(xlabel)
+ax.set_xscale(log_x)
+
+# As fraction of all-rain-total
+ax.set_ylabel('LW-ACRE * Fraction')
+total=np.sum(binvar_s_mn[:,(1,2,3)])
+plt.plot(bins[0:nbins-1], binvar_acre_mn*binvar_s_mn[:,0]/total \
+         , ".k", label="Non-raining")
+plt.plot(bins[0:nbins-1], binvar_acre_mn*binvar_s_mn[:,1]/total \
+         , "-r", label="Conv")
+plt.plot(bins[0:nbins-1], binvar_acre_mn*binvar_s_mn[:,2]/total \
+         , "-b", label="Strat")
+plt.plot(bins[0:nbins-1], binvar_acre_mn*binvar_s_mn[:,3]/total \
+         , "--b", label="Other")
+plt.ylim(0, 10)
+
+plt.legend(loc="upper left")
+
+# plt.show()
+plt.savefig(figdir+'lwacrescaled_comp_'+imemb+'_'+ivar_select+'.png',dpi=200, facecolor='white')
 
