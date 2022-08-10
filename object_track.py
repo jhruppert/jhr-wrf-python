27bf8d3c08f7274b1a0e10744755f6199a233f66
James Ruppert <jruppert.jr@gmail.com>
2022-08-09 23:00:58 -0500

    updates, tracking script complete

--- object_track.py
+++ object_track.py
@@ -27,7 +27,7 @@ import numpy as np
 from scipy import ndimage
 import sys
 
-def object_track(f, lon, lat, nx_sm):
+def object_track(f, lon, lat):
 
     shape=np.shape(f)
     nt,nx,ny = shape
@@ -41,77 +41,94 @@ def object_track(f, lon, lat, nx_sm):
 
     # SETTINGS
 
-    # nx_sm=9    # Following Chavas 2013 (XX smoothing run x30 times)
-    n_repeat=3 # Run smoothing n-times
-
-    # MAXIMUM RADIUS RANGE FROM TIME-SPACE MAX
-    r_max=5 # Masked out beyond this radius at the neighboring time steps [degrees]
-        # ^ also used to determine buffer from boundaries
-    dxkm=3 # Grid spacing [km]
-
-    # CONSTANTS
-    # m2deg=1./(111e3)
-    # THE BELOW ARE FOR TRACKING VORTEX SUBJECT TO C_MAX
-        # dt=(time[1]-time[0])*86400.d # s
-        # idt=1d/dt
-        # rmax_track = c_max * dt * m2deg # maximum single-time-step displacement [degrees]
+    nx_sm=9      # Following Chavas 2013 (XX smoothing run x30 times)
+    nx_repeat=15 # Run x-smoothing n-times
+    nt_smooth=3  # Time-step smoothing
+    # nt_repeat=3  # Run time-smoothing n-times
+    r_max=5      # Masked out beyond this radius [degrees] at adjacent time steps
+                 # Also used to determine buffer from boundaries
 
     # 3-dimensional lon/lat for weighting
     lon3d = np.repeat(lon[np.newaxis,:,:], nt, axis=0)
     lat3d = np.repeat(lat[np.newaxis,:,:], nt, axis=0)
 
-    # OBJECT TRACKING SUBJECT TO A MAXIMUM ALLOWABLE TRANSLATION SPEED
-    # m2deg=1./(111e3)
-    # c_max=40. # A maximum allowable translation speed (should be set to something very liberal)
-    # dt=3600.  # Data time step [seconds]
-    # idt=1./dt
-    # rmax_track = c_max * dt * m2deg ; maximum single-time-step displacement [degrees]
-
     #############################################
 
-    # SMOOTHING AND MASKING
+    # SMOOTHING
 
     # Smooth input variable in x,y
     f_smooth = ndimage.uniform_filter(f,size=(0,nx_sm,nx_sm),mode='nearest')
-    for ido in range(n_repeat-1):
+    for ido in range(nx_repeat-1):
         f_smooth = ndimage.uniform_filter(f_smooth,size=(0,nx_sm,nx_sm),mode='nearest')
 
     # Smooth input variable in time
-    # n_repeat=3
-    nt_smooth=3
-    # Smooth input variable
-    # for ido in range(n_repeat):
+    # for ido in range(nt_repeat):
     f_smooth = ndimage.uniform_filter(f_smooth,size=(nt_smooth,0,0),mode='nearest')
+
+    # BULK MASKING
 
     # Mask out values < 3 sigma
     f_sigma = f_smooth / np.std(f_smooth)
     f_masked = np.ma.array(f_sigma)
-    f_masked = np.ma.masked_where(np.abs(f_masked) < 3, f_masked) #copy=True)
+    f_masked = np.ma.masked_where(np.abs(f_masked) < 3, f_masked, copy=False)
+
+    # Mask out first time step
+    f_masked[0,:,:] = np.nan
+    f_masked = np.ma.masked_invalid(f_masked, copy=False)
 
     # Mask out data within 0.5*r_max from boundaries
-    nxmask = round( 0.5*r_max*111./dxkm )
-    f_masked = np.ma.masked_where(lon3d <= lon1d[nxmask], f_masked, copy=True)
-    f_masked = np.ma.masked_where(lon3d >= lon1d[nx-nxmask-1], f_masked, copy=True)
-    f_masked = np.ma.masked_where(lat3d <= lat1d[nxmask], f_masked, copy=True)
-    f_masked = np.ma.masked_where(lat3d >= lat1d[ny-nxmask-1], f_masked, copy=True)
-
-    # Assurance
-    # print(np.min(f_sigma))
-    # print(np.max(f_sigma))
-    # print(np.min(np.abs(f_sigma)))
-    # print()
-    # print(np.min(f_masked))
-    # print(np.max(f_masked))
-    # print(np.min(np.abs(f_masked)))
+    f_masked = np.ma.masked_where(lon3d <= lon1d[0]+0.5*r_max, f_masked, copy=False)
+    f_masked = np.ma.masked_where(lon3d >= lon1d[nx-1]-0.5*r_max, f_masked, copy=False)
+    f_masked = np.ma.masked_where(lat3d <= lat1d[0]+0.5*r_max, f_masked, copy=False)
+    f_masked = np.ma.masked_where(lat3d >= lat1d[ny-1]-0.5*r_max, f_masked, copy=False)
 
     #############################################
 
-    # Track maxima in time by finding the centroid
+    # MASK BEYOND SPECIFIED RADIUS STARTING FROM ALL-TIME MAX
 
-    lon_tim = np.repeat(lon[np.newaxis,:,:], nt, axis=0)
-    lat_tim = np.repeat(lat[np.newaxis,:,:], nt, axis=0)
+    # Locate the all-time maximum value
+    fmax = np.max(f_masked)
+    mloc=np.where(f_masked == fmax)
+    itmax = mloc[0][0]
+    xmax=mloc[2][0]
+    ymax=mloc[1][0]
 
-    clon = np.average(lon_tim,axis=(1,2),weights=f_masked)
-    clat = np.average(lat_tim,axis=(1,2),weights=f_masked)
+    radius = np.sqrt( (lon-lon1d[xmax])**2 + (lat-lat1d[ymax])**2 )
 
-    return f_masked
+    # Mask surrounding points
+    for it in range(itmax-1,np.minimum(itmax+1,nt-1)+1):
+        f_masked[it,:,:] = np.ma.masked_where(radius > r_max, f_masked[it,:,:], copy=False)
+
+    # Iterate downward from itmax
+    for it in range(itmax-1,1,-1):
+
+        fmax = np.max(f_masked[it,:,:])
+        mloc = np.where(f_masked[it,:,:] == fmax)
+        xmax = mloc[1][0]
+        ymax = mloc[0][0]
+
+        radius = np.sqrt( (lon-lon1d[xmax])**2 + (lat-lat1d[ymax])**2 )
+        f_masked[it-1,:,:] = np.ma.masked_where(radius > r_max, f_masked[it-1,:,:], copy=False)
+
+    # Iterate upward from itmax
+    for it in range(itmax+1,nt-1):
+
+        fmax = np.max(f_masked[it,:,:])
+        mloc = np.where(f_masked[it,:,:] == fmax)
+        xmax = mloc[1][0]
+        ymax = mloc[0][0]
+
+        radius = np.sqrt( (lon-lon1d[xmax])**2 + (lat-lat1d[ymax])**2 )
+        f_masked[it+1,:,:] = np.ma.masked_where(radius > r_max, f_masked[it+1,:,:], copy=False)
+
+    #############################################
+
+    # TRACKING
+
+    # Track maxima in time as the centroid
+    clon = np.average(lon3d,axis=(1,2),weights=f_masked)
+    clat = np.average(lat3d,axis=(1,2),weights=f_masked)
+
+    track=np.stack([clon,clat])
+
+    return track
