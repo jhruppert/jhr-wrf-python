8125e7f7eb671f32ed92d3327504f24cf5a346c0
James Ruppert <jruppert.jr@gmail.com>
2022-10-03 00:57:37 -0500

    updates

--- azim_wind_latlon.py
+++ azim_wind_latlon.py
@@ -21,7 +21,14 @@
 # from netCDF4 import Dataset
 # import numpy as np
 
-def azim_wind_latlon(track_file, u, v, lon, lat, rmax):
+from netCDF4 import Dataset
+import numpy as np
+
+def azim_wind_latlon(track_file, u, v, lon, lat, rmax, t0, t1):
+
+    # Input dimensions
+    nt,nz,nx1,nx2 = u.shape
+
 
     # Read TC track
     ncfile = Dataset(track_file)
