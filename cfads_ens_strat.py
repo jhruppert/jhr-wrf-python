dccc566bf608c20852b58a20e8913eb2df044620
James Ruppert <jruppert.jr@gmail.com>
2022-11-11 09:00:32 -0600

    updates

--- cfads_ens_strat.py
+++ cfads_ens_strat.py
@@ -30,7 +30,7 @@ from mask_tc_track import mask_tc_track
 iplot = 'thv'#'the'#'vmf'#'rh'#'qrad'#
 # iplot = 'qrad'
 iplot = 'vmf'
-# iplot = 'rh'
+iplot = 'rh'
 # options: vmf, thv, the
 
 # Calculate anomaly as deviation from xy-mean
@@ -39,8 +39,8 @@ do_prm_xy = 0
 do_prm_inc = 0
 
 # istrat=2 # 0-non-raining, 1-conv, 2-strat, 3-other/anvil, (-1 for off)
-# for istrat in range(-1,3):
-for istrat in range(1,3):
+for istrat in range(-1,3):
+# for istrat in range(1,3):
   
   print("Strat = ",istrat)
   # continue
