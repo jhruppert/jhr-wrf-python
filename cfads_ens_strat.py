b805eda4b52fa63f1d84e5c1ec9cd430da29e14e
James Ruppert <jruppert.jr@gmail.com>
2023-05-19 18:21:24 -0500

    updates

--- cfads_ens_strat.py
+++ cfads_ens_strat.py
@@ -26,7 +26,6 @@ from cfads_functions import cfads_var_settings, cfads_var_calc
 
 # How many ensemble members
 nmem = 10 # number of ensemble members (1-10 have NCRF)
-# nmem = 5
 # nmem = 3
 enstag = str(nmem)
 
@@ -41,7 +40,8 @@ ivar_all = ['wpthp','wpthep']
 ivar_all = ['lh','thv','the']
 ivar_all = ['lh','vmf']
 ivar_all = ['lq']
-ivar_all = ['lh']
+ivar_all = ['lh','vmf']
+ivar_all = ['vmf']
 nvar=np.size(ivar_all)
 
 # #### Time selection
@@ -50,8 +50,15 @@ nvar=np.size(ivar_all)
 # ntall=[1,3,6,12]
 # ntall=[1,6,12]
 ntall=[1,2,3,6]
-ntall=[1,3]
+ntall=[1,6]
+ntall=[1]
 # ntall=[1,3,6]
+
+# #### Classification selection
+
+# 0-non-raining, 1-conv, 2-strat, 3-other/anvil, (-1 for off)
+# kclass=[0,1,2,3]
+kclass=[2]
 
 # #### Storm selection
 
@@ -87,236 +94,248 @@ for ivar in range(nvar):
       do_prm_xy=0
 
 
-  # istrat=2 # 0-non-raining, 1-conv, 2-strat, 3-other/anvil, (-1 for off)
-  # for istrat in range(-1,4):
-  # for istrat in range(2,3):
-  for istrat in range(-1,3):
+  for istorm in range(nstorm):
 
-    for istorm in range(nstorm):
+    storm=storm_all[istorm]
+    print("Storm: ",storm)
 
-      storm=storm_all[istorm]
-      print("Storm: ",storm)
+    # Tests to compare
+    if storm == 'haiyan':
+      tests = ['ctl','ncrf36h']
+    elif storm == 'maria':
+      # tests = ['ctl','ncrf36h']
+      tests = ['ctl','ncrf48h']
+    # tests = ['crfon','ncrf']
 
-      # Tests to compare
-      if storm == 'haiyan':
-        tests = ['ctl','ncrf36h']
-      elif storm == 'maria':
-        # tests = ['ctl','ncrf36h']
-        tests = ['ctl','ncrf48h']
-      # tests = ['crfon','ncrf']
+    # Shift starting-read time step for CRFON comparison
+    t0_test=0
 
-      # Shift starting-read time step for CRFON comparison
-      t0_test=0
+    # #### Time selection
+    i_nt=np.shape(ntall)[0]
 
-      # Strat/Conv index subset
-      if istrat == -1:
-        fig_extra=''
+    for knt in range(i_nt):
+    # for knt in range(0,1):
+    #for knt in range(3,i_nt+1):
+
+      nt = ntall[knt]
+      hr_tag = str(np.char.zfill(str(nt), 2))
+      print("Hour sample: ",hr_tag)
+
+
+      # #### Directories
+
+      figdir = "/home/jamesrup/figures/tc/ens/"+storm+'/'
+      # main = "/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/wrfenkf/"
+      main = "/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/tc_ens/"
+
+      memb0=1
+      nums=np.arange(memb0,nmem+memb0,1); nums=nums.astype(str)
+      nustr = np.char.zfill(nums, 2)
+      memb_all=np.char.add('memb_',nustr)
+
+      # datdir2 = 'post/d02/v2/'
+      datdir2 = 'post/d02/'
+
+
+      ##### Get dimensions
+
+      datdir = main+storm+'/'+memb_all[0]+'/'+tests[0]+'/'+datdir2
+      varfil_main = Dataset(datdir+'T.nc')
+      nz = varfil_main.dimensions['level'].size
+      # lat = varfil_main.variables['XLAT'][:][0] # deg
+      # lon = varfil_main.variables['XLONG'][:][0] # deg
+      nx1 = varfil_main.dimensions['lat'].size
+      nx2 = varfil_main.dimensions['lon'].size#-xmin-1
+      pres = varfil_main.variables['pres'][:] # hPa
+      varfil_main.close()
+
+      process = subprocess.Popen(['ls '+main+storm+'/'+memb_all[0]+'/'+tests[0]+'/wrfout_d02_*'],shell=True,
+          stdout=subprocess.PIPE,universal_newlines=True)
+      output = process.stdout.readline()
+      wrffil = output.strip() #[3]
+      varfil_main = Dataset(wrffil)
+      lat = varfil_main.variables['XLAT'][:][0] # deg
+      lon = varfil_main.variables['XLONG'][:][0] # deg
+      varfil_main.close()
+
+      # Get variable settings
+      bins, fig_title, fig_tag, units_var, scale_mn, \
+        units_mn, xrange_mn, xrange_mn2 = cfads_var_settings(iplot)
+      nbin=np.shape(bins)[0]
+
+      # Create axis of bin center-points
+      bin_axis = (bins[np.arange(nbin-1)]+bins[np.arange(nbin-1)+1])/2
+
+      if do_prm_xy == 1:
+          fig_tag+='_xyp'
+          fig_title+="$'$"# (xp)'
+      if do_prm_inc == 1:
+          fig_tag+='_tp'
+          fig_title+=' (tp)'
+
+
+      # #### Read variables ##############################################
+      
+      ntest=2
+      var_freq=np.ma.zeros((ntest,nbin-1,nz))
+      var_freq_int=np.ma.zeros((ntest,nbin-1))
+
+      # Create arrays to save ens members
+      if do_prm_inc == 1:
+        var_all = np.ma.zeros((ntest,nmem,nt+1,nz,nx1,nx2)) # time dim will be reduced to nt in the subtraction
       else:
-        if istrat == 0:
-          strattag='Nonrain'
-        elif istrat == 1:
-          strattag='Conv'
-        elif istrat == 2:
-          strattag='Strat'
-        elif istrat == 3:
-          # strattag='Anv'
-          strattag='CS'
-        fig_extra='_'+strattag.lower()
-        print("Strat tag: ",strattag)
+        var_all = np.ma.zeros((ntest,nmem,nt,nz,nx1,nx2))
+        # var_copy = np.ma.zeros((ntest,nmem,nt,nz,nx1,nx2))
+        strat_all = np.ma.zeros((ntest,nmem,nt,1,nx1,nx2))
 
-      # #### Time selection
-      i_nt=np.shape(ntall)[0]
+      for ktest in range(ntest):
+      
+        itest=tests[ktest]
 
-      for knt in range(i_nt):
-      # for knt in range(0,1):
-      #for knt in range(3,i_nt+1):
+        # if ktest == 0:
+        #   nmem=1
+        # else:
+        #   nmem=5
 
-        nt = ntall[knt]
-        hr_tag = str(np.char.zfill(str(nt), 2))
-        print("Hour sample: ",hr_tag)
+        # This has been tested for corresponding time steps:
+        #   t0=37,1 are the first divergent time steps in CTL,NCRF
+        #   t0=25,1 are the first divergent time steps in NCRF,CRFON
+        if itest == 'ctl':
+          if tests[1] == 'ncrf36h':
+            t0=36
+          elif tests[1] == 'ncrf48h':
+            t0=48
+        elif itest == 'ncrf36h':
+          t0=t0_test
+        elif itest == 'ncrf48h':
+          t0=t0_test
+        elif itest == 'crfon':
+          t0=0
 
+        if do_prm_inc == 0:
+          t0+=1 # add one time step since NCRF(t=0) = CTL
 
-        # #### Directories
-
-        figdir = "/home/jamesrup/figures/tc/ens/"+storm+'/'
-        # main = "/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/wrfenkf/"
-        main = "/ourdisk/hpc/radclouds/auto_archive_notyet/tape_2copies/tc_ens/"
-
-        memb0=1
-        nums=np.arange(memb0,nmem+memb0,1); nums=nums.astype(str)
-        nustr = np.char.zfill(nums, 2)
-        memb_all=np.char.add('memb_',nustr)
-
-        # datdir2 = 'post/d02/v2/'
-        datdir2 = 'post/d02/'
-
-
-        ##### Get dimensions
-
-        datdir = main+storm+'/'+memb_all[0]+'/'+tests[0]+'/'+datdir2
-        varfil_main = Dataset(datdir+'T.nc')
-        nz = varfil_main.dimensions['level'].size
-        # lat = varfil_main.variables['XLAT'][:][0] # deg
-        # lon = varfil_main.variables['XLONG'][:][0] # deg
-        nx1 = varfil_main.dimensions['lat'].size
-        nx2 = varfil_main.dimensions['lon'].size#-xmin-1
-        pres = varfil_main.variables['pres'][:] # hPa
-        varfil_main.close()
-
-        process = subprocess.Popen(['ls '+main+storm+'/'+memb_all[0]+'/'+tests[0]+'/wrfout_d02_*'],shell=True,
-            stdout=subprocess.PIPE,universal_newlines=True)
-        output = process.stdout.readline()
-        wrffil = output.strip() #[3]
-        varfil_main = Dataset(wrffil)
-        lat = varfil_main.variables['XLAT'][:][0] # deg
-        lon = varfil_main.variables['XLONG'][:][0] # deg
-        varfil_main.close()
-
-        # Get variable settings
-        bins, fig_title, fig_tag, units_var, scale_mn, \
-          units_mn, xrange_mn, xrange_mn2 = cfads_var_settings(iplot)
-        nbin=np.shape(bins)[0]
-
-        # Create axis of bin center-points
-        bin_axis = (bins[np.arange(nbin-1)]+bins[np.arange(nbin-1)+1])/2
-
-        if do_prm_xy == 1:
-            fig_tag+='_xyp'
-            fig_title+="$'$"# (xp)'
+        t1 = t0+nt
         if do_prm_inc == 1:
-            fig_tag+='_tp'
-            fig_title+=' (tp)'
+          t1+=1
 
+        print('Running itest: ',itest)
 
-        # #### Read variables ##############################################
+        for imemb in range(nmem):
+      
+          print('Running imemb: ',memb_all[imemb])
+      
+          datdir = main+storm+'/'+memb_all[imemb]+'/'+itest+'/'+datdir2
+          print(datdir)
+      
+          # Localize to TC track
+          # track_file = datdir+'../../../track_'+var_track+'_'+ptrack+'hPa.nc'
+          # track_file = datdir+'../../track_'+var_track+'_'+ptrack+'hPa.nc'
+          # Localize to TC track
+          # NOTE: Using copied tracking from CTL for NCRF tests
+          # track_file = datdir+'../../track_'+var_track+'_'+ptrack+'hPa.nc'
+          trackfil_ex=''
+          if 'ncrf' in itest:
+              trackfil_ex='_ctlcopy'
+          track_file = datdir+'../../track_'+var_track+trackfil_ex+'_'+ptrack+'hPa.nc'
+
+        # Two-dimensional variables
+
+        # Stratiform index
+          # if istrat != -1:
+          varfil_main = Dataset(datdir+'strat.nc')
+          strat = varfil_main.variables['strat'][t0:t1,:,:,:] # 0-non-raining, 1-conv, 2-strat, 3-other/anvil
+          varfil_main.close()
+
+        # Three-dimensional variables
+          var = cfads_var_calc(iplot, datdir, pres, t0, t1)
+
+        ### Process variable ##############################################
+
+          # Calculate var' as anomaly from x-y-average, using large-scale (large-radius) var avg
+          if do_prm_xy == 1:
+            radius_ls=6
+            var_ls = mask_tc_track(track_file, radius_ls, var, lon, lat, t0, t1)
+            # var_ls = mask_tc_track(track_file, rmax, var, lon, lat, t0, t1)
+            var_ls_avg = np.ma.mean(var_ls,axis=(0,2,3))
+            var -= var_ls_avg[np.newaxis,:,np.newaxis,np.newaxis]
+
+          # Calculate w'th'
+          if ('wpth' in iplot):
+            print("NOT RUNNING THIS VAR. NEED TO FIX READ FUNCTION")
+            print("PUNTING TO FUTURE SELF WHO IS WISER THAN I")
+            sys.exit()
+            # rmax_prime = rmax
+            # var_ls1 = mask_tc_track(track_file, rmax_prime, thp, lon, lat, t0, t1)
+            # var_ls1_avg = np.ma.mean(var_ls1,axis=(0,2,3))
+            # thp -= var_ls1_avg[np.newaxis,:,np.newaxis,np.newaxis]
+            # var_ls2 = mask_tc_track(track_file, rmax_prime, www, lon, lat, t0, t1)
+            # var_ls2_avg = np.ma.mean(var_ls2,axis=(0,2,3))
+            # www -= var_ls2_avg[np.newaxis,:,np.newaxis,np.newaxis]
+            # var = thp*www
+
+          # # Mask out based on strat/conv
+          # if (istrat != -1) & (istrat != 3):
+          #   var = np.ma.masked_where((np.repeat(strat,nz,axis=1) != istrat), var, copy=True)
+          #   # vmf_copy = np.ma.masked_where((np.repeat(strat,nz,axis=1) != istrat), vmf_copy, copy=True)
+          # elif istrat == 3:
+          #   var = np.ma.masked_where(((np.repeat(strat,nz,axis=1) == 0) & (np.repeat(strat,nz,axis=1) == 3)),
+          #       var, copy=True)
+
+          # Localize to TC track
+          var = mask_tc_track(track_file, rmax, var, lon, lat, t0, t1)
+          # vmf_copy = mask_tc_track(track_file, rmax, vmf_copy, lon, lat, t0, t1)
+
+          # Save ens member
+          var_all[ktest,imemb,:,:,:,:] = var
+          strat_all[ktest,imemb,:,:,:,:] = strat
+          # var_copy[imemb,:,:,:,:] = vmf_copy
+
+      # Calculate var' as time-increment: var[t] - var[t-1]
+      if do_prm_inc == 1:
+        var_all = var_all[:,:,1:,:,:,:] - var_all[:,:,:-1,:,:,:]
+
+##### CLASSIFICATION LOOP ###############
+
+      # 0-non-raining, 1-conv, 2-strat, 3-other/anvil, (-1 for off)
+      nclass=np.shape(kclass)[0]
+      for kstrat in range(nclass):
         
-        ntest=2
-        var_freq=np.ma.zeros((ntest,nbin-1,nz))
-        var_freq_int=np.ma.zeros((ntest,nbin-1))
-        var_mn=np.ma.zeros((ntest,nz))
-        
-        for ktest in range(ntest):
-        
-          itest=tests[ktest]
+        istrat=kclass[kstrat]
 
-          # if ktest == 0:
-          #   nmem=1
-          # else:
-          #   nmem=5
+        # Strat/Conv index subset
+        if istrat == -1:
+          fig_extra=''
+        else:
+          if istrat == 0:
+            strattag='Nonrain'
+          elif istrat == 1:
+            strattag='Conv'
+          elif istrat == 2:
+            strattag='Strat'
+          elif istrat == 3:
+            # strattag='Anv'
+            strattag='CS'
+          fig_extra='_'+strattag.lower()
+          print("Strat tag: ",strattag)
 
-          # This has been tested for corresponding time steps:
-          #   t0=37,1 are the first divergent time steps in CTL,NCRF
-          #   t0=25,1 are the first divergent time steps in NCRF,CRFON
-          if itest == 'ctl':
-            if tests[1] == 'ncrf36h':
-              t0=36
-            elif tests[1] == 'ncrf48h':
-              t0=48
-          elif itest == 'ncrf36h':
-            t0=t0_test
-          elif itest == 'ncrf48h':
-            t0=t0_test
-          elif itest == 'crfon':
-            t0=0
-
-          if do_prm_inc == 0:
-            t0+=1 # add one time step since NCRF(t=0) = CTL
-
-          t1 = t0+nt
-          if do_prm_inc == 1:
-            t1+=1
-
-          print('Running itest: ',itest)
-
-          # Create arrays to save ens members
-          if do_prm_inc == 1:
-            var_all = np.ma.zeros((nmem,nt+1,nz,nx1,nx2)) # time dim will be reduced to nt in the subtraction
-          else:
-            var_all = np.ma.zeros((nmem,nt,nz,nx1,nx2))
-            # var_copy = np.ma.zeros((nmem,nt,nz,nx1,nx2))
-
-          for imemb in range(nmem):
-        
-            print('Running imemb: ',memb_all[imemb])
-        
-            datdir = main+storm+'/'+memb_all[imemb]+'/'+itest+'/'+datdir2
-            print(datdir)
-        
-            # Localize to TC track
-            # track_file = datdir+'../../../track_'+var_track+'_'+ptrack+'hPa.nc'
-            # track_file = datdir+'../../track_'+var_track+'_'+ptrack+'hPa.nc'
-            # Localize to TC track
-            # NOTE: Using copied tracking from CTL for NCRF tests
-            # track_file = datdir+'../../track_'+var_track+'_'+ptrack+'hPa.nc'
-            trackfil_ex=''
-            if 'ncrf' in itest:
-                trackfil_ex='_ctlcopy'
-            track_file = datdir+'../../track_'+var_track+trackfil_ex+'_'+ptrack+'hPa.nc'
-
-          # Two-dimensional variables
-
-          # Stratiform index
-            if istrat != -1:
-              varfil_main = Dataset(datdir+'strat.nc')
-              strat = varfil_main.variables['strat'][t0:t1,:,:,:] # 0-non-raining, 1-conv, 2-strat, 3-other/anvil
-              varfil_main.close()
-
-          # Three-dimensional variables
-            var = cfads_var_calc(iplot, datdir, pres, t0, t1)
-
-          ### Process variable ##############################################
-
-            # Calculate var' as anomaly from x-y-average, using large-scale (large-radius) var avg
-            if do_prm_xy == 1:
-              radius_ls=6
-              var_ls = mask_tc_track(track_file, radius_ls, var, lon, lat, t0, t1)
-              # var_ls = mask_tc_track(track_file, rmax, var, lon, lat, t0, t1)
-              var_ls_avg = np.ma.mean(var_ls,axis=(0,2,3))
-              var -= var_ls_avg[np.newaxis,:,np.newaxis,np.newaxis]
-
-            # Calculate w'th'
-            if ('wpth' in iplot):
-              print("NOT RUNNING THIS VAR. NEED TO FIX READ FUNCTION")
-              print("PUNTING TO FUTURE SELF WHO IS WISER THAN I")
-              sys.exit()
-              # rmax_prime = rmax
-              # var_ls1 = mask_tc_track(track_file, rmax_prime, thp, lon, lat, t0, t1)
-              # var_ls1_avg = np.ma.mean(var_ls1,axis=(0,2,3))
-              # thp -= var_ls1_avg[np.newaxis,:,np.newaxis,np.newaxis]
-              # var_ls2 = mask_tc_track(track_file, rmax_prime, www, lon, lat, t0, t1)
-              # var_ls2_avg = np.ma.mean(var_ls2,axis=(0,2,3))
-              # www -= var_ls2_avg[np.newaxis,:,np.newaxis,np.newaxis]
-              # var = thp*www
-
-            # Mask out based on strat/conv
-            if (istrat != -1) & (istrat != 3):
-              var = np.ma.masked_where((np.repeat(strat,nz,axis=1) != istrat), var, copy=True)
-              # vmf_copy = np.ma.masked_where((np.repeat(strat,nz,axis=1) != istrat), vmf_copy, copy=True)
-            elif istrat == 3:
-              var = np.ma.masked_where(((np.repeat(strat,nz,axis=1) == 0) & (np.repeat(strat,nz,axis=1) == 3)),
-                  var, copy=True)
-
-            # Localize to TC track
-            var = mask_tc_track(track_file, rmax, var, lon, lat, t0, t1)
-            # vmf_copy = mask_tc_track(track_file, rmax, vmf_copy, lon, lat, t0, t1)
-
-            # Save ens member
-            var_all[imemb,:,:,:,:] = var
-            # var_copy[imemb,:,:,:,:] = vmf_copy
+        # Mask out based on strat/conv
+        if ((istrat != -1) & (istrat != 3)):
+          var_ma = np.ma.masked_where((np.repeat(strat_all,nz,axis=3) != istrat), var_all, copy=True)
+          # vmf_copy = np.ma.masked_where((np.repeat(strat,nz,axis=1) != istrat), vmf_copy, copy=True)
+        elif istrat == 3:
+          var_ma = np.ma.masked_where(((np.repeat(strat_all,nz,axis=3) == 0) & (np.repeat(strat_all,nz,axis=3) == 3)),
+              var_all, copy=True)
 
         #### Calculate basic mean
-          var_mn[ktest,:]=np.ma.mean(var_all, axis=(0,1,3,4))
+        var_mn=np.ma.mean(var_ma, axis=(1,2,4,5))
 
-        # Calculate var' as time-increment: var[t] - var[t-1]
-          if do_prm_inc == 1:
-            var_all = var_all[:,1:,:,:,:] - var_all[:,:-1,:,:,:]
-        
-        
-        #### Calculate frequency ##############################################
+#### Calculate frequency ##############################################
 
+        for ktest in range(ntest):
           for iz in range(nz):
             for ibin in range(nbin-1):
-              indices = ((var_all[:,:,iz,:,:] >= bins[ibin]) & (var_all[:,:,iz,:,:] < bins[ibin+1])).nonzero()
+              indices = ((var_ma[ktest,:,:,iz,:,:] >= bins[ibin]) & (var_ma[ktest,:,:,iz,:,:] < bins[ibin+1])).nonzero()
               var_freq[ktest,ibin,iz]=np.shape(indices)[1]
             var_freq[ktest,:,iz] /= np.ma.sum(var_freq[ktest,:,iz])
           #ncell=nx1*nx2*nt*nmem
